147dc4b2c4c0ed2cfcd7cda0b3f87c39d6e12457
WarFox (Deepu Mohan Puthrote) <git@deepumohan.com>
2017-03-06 10:16:54 +0000

    Calculate subtotal from a given pricelist

--- src/main/java/com/anatwine/shop/BillGenerator.java
+++ src/main/java/com/anatwine/shop/BillGenerator.java
@@ -1,14 +1,31 @@
 package com.anatwine.shop;
 
+import com.anatwine.shop.models.Product;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This is the main class that generates a bill
  */
 public class BillGenerator {
 
+    private Map<String, Double> priceList;
+
+    public BillGenerator(Map<String, Double> priceList) {
+       this.priceList = priceList;
+    }
+
     public Bill generateBill(ShoppingCart shoppingCart) {
-        return new Bill(0.0, 0.0, new ArrayList<>());
+        List<String> items = shoppingCart.getItems().stream().map(Product::getName).collect(Collectors.toList());
+        Double subTotal = calculateSubTotal(items, priceList);
+        return new Bill(subTotal, 0.0, new ArrayList<>());
+    }
+
+    public Double calculateSubTotal(List<String> items, Map<String, Double> priceList) {
+        return items.stream().mapToDouble(priceList::get).sum();
     }
 
 }
--- src/test/java/com/anatwine/shop/BillGeneratorTest.java
+++ src/test/java/com/anatwine/shop/BillGeneratorTest.java
@@ -1,19 +1,36 @@
 package com.anatwine.shop;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BillGeneratorTest {
 
+    private static final Map<String, Double> priceList = new HashMap<>();
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        priceList.put("Apple", 1.30);
+        priceList.put("Orange", 1.30);
+    }
+
     @Test
     public void testGenerateBillWithOutOffers() throws Exception {
         ShoppingCart shoppingCart = new ShoppingCart(new ArrayList<>());
-        BillGenerator billGenerator = new BillGenerator();
+        BillGenerator billGenerator = new BillGenerator(priceList);
         Bill bill = billGenerator.generateBill(shoppingCart);
         assertThat(bill.getSubtotal()).isZero();
+    }
+
+    @Test
+    public void testCalculateSubtotal() throws Exception {
+        BillGenerator billGenerator = new BillGenerator(priceList);
+        List<String> items = Arrays.asList("Apple", "Orange", "Apple", "Orange");
+        Double subTotal = billGenerator.calculateSubTotal(items, priceList);
+        assertThat(subTotal).isEqualTo(5.20);
     }
 
 }
